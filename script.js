2fbd71808a720c1262b5474ec6a8374a9bbb6e66
Abel kabeya <abelkofficial@gmail.com>
2022-04-05 11:24:06 +0200

    getting the elements in JS

---
+++ script.js
@@ -0,0 +1,7 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentwatherItensEl = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryEl = document.getElementById('country');
+const weatherForecastEl = document.getElementById('weather.forecast');
+const currentTemp = document.getElementById('current-temp');
