2bd3329d7da6afb9ee730ad56ad1be7229f05250
Abel kabeya <abelkofficial@gmail.com>
2022-04-05 11:47:25 +0200

    updating the date and time values

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const timeEl = document.getElementById('time');
 const dateEl = document.getElementById('date');
-const currentwatherItensEl = document.getElementById('current-weather-items');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
 const timezone = document.getElementById('time-zone');
 const countryEl = document.getElementById('country');
-const weatherForecastEl = document.getElementById('weather.forecast');
-const currentTemp = document.getElementById('current-temp');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+setInterval(() =>{
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    timeEl.innerHTML =hoursInt12HrFormat + ':' + minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
+
+    dateEl.innerHTML = days[day] + ', ' + date+ ' ' +months[month]
+
+}, 1000);
