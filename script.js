e447949c97d1f93ede3a234ad5de702a1a6b76be
Abel kabeya <abelkofficial@gmail.com>
2022-04-05 12:27:15 +0200

    calling the Open weather API

--- script.js
+++ script.js
@@ -10,17 +10,37 @@ const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 
 const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 
+const API_KEY = 'fa4ceaff7aeb4727431335817c91674d';
+
 setInterval(() =>{
     const time = new Date();
     const month = time.getMonth();
     const date = time.getDate();
     const day = time.getDay();
     const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour %12: hour
     const minutes = time.getMinutes();
     const ampm = hour >=12 ? 'PM' : 'AM'
 
-    timeEl.innerHTML =hoursInt12HrFormat + ':' + minutes+ ' ' + `<span id="am-pm">${ampm}</span>`
-
+    timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0'+minutes: minutes)+ ' ' + `<span id="am-pm">${ampm}</span>`
     dateEl.innerHTML = days[day] + ', ' + date+ ' ' +months[month]
 
 }, 1000);
+
+getWeatherData()
+function getWeatherData(){
+    navigator.geolocation.getCurrentPosition((success) => {
+    
+        let {latitude, longitude} = success.coords;
+
+        fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+        console.log(data)
+        showWeatherData(data);
+        })
+    })
+}
+
+function showWeatherData(data){
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+}
